1f5e588dedd7c6f340542eb825818e9c2d49bbdc
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2023-11-04 05:00:44 +0200

    Refactor createSlot

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -3,5 +3,8 @@ const doctorService = require('../services/DoctorService.js')
 
 const router = require('express').Router()
 
+router.post('/createSlot', doctorService.createSlot )
+
+
 
 module.exports = router
