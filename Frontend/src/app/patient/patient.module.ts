4e46419ac3bd621e3491cd1c395f6728e69ff298
FatmaAhmed25 <fatmaahmed6555@gmail.com>
2023-11-11 01:06:29 +0200

    deletion

--- Frontend/src/app/patient/patient.module.ts
+++ Frontend/src/app/patient/patient.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
 import { FormsModule } from '@angular/forms';
 import { PatientHomePageComponent } from './patient-home-page/patient-home-page.component';
-import { PatientDashboard2Component } from './patientdashboard2/patientdashboard2.component';
+
 import { CancelConfirmationDialogComponent } from './cancel-confirmation-dialog/cancel-confirmation-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 
@@ -13,7 +13,7 @@ import { MatDialogModule } from '@angular/material/dialog';
   declarations: [
     PatientDashboardComponent,
     PatientHomePageComponent,
-    PatientDashboard2Component,
+
     CancelConfirmationDialogComponent
   ],
   imports: [
