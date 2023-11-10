4b2b6de187556e3f8b30a18598c9dced8d5f9e6f
sarahyasser <sarahyasserxo@gmail.com>
2023-11-11 00:58:22 +0200

    logout-navbar

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+    menuType:String ='default';
+    constructor(private route:Router){}
+
+    ngOnInit(): void {
+      this.route.events.subscribe((val: any) => {
+        if (val.url) {
+          console.warn(val.url);
     
-}
+          if (val.url.includes('doctor')) {
+            console.warn('herree');
+            this.menuType = 'doctor';
+          } 
+          else if(val.url.includes('patient')){
+            console.warn('noo');
+            this.menuType='patient';
+          }
+        }
+      });
+    
+  }
+    }
+
